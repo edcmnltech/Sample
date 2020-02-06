f08322a1374afbdde49b1084aada260012da2a7e
Elizer Dela Cruz <elizer.delacruz@mnltechnology.com>
2020-02-06 16:39:40 +0800

    adding of nickname

--- src/main/scala/com/sample/chat/User.scala
+++ src/main/scala/com/sample/chat/User.scala
@@ -4,25 +4,25 @@ import akka.actor.{Actor, ActorRef}
 import com.sample.chat.User.{Connected, IncomingMessage, OutgoingMessage}
 
 object User {
-  final case class Connected(nickname: String, outgoing: ActorRef)
-  final case class IncomingMessage(author: String, text: String)
-  final case class OutgoingMessage(author: String, text: String)
+  final case class Connected(outgoing: ActorRef)
+  final case class IncomingMessage(text: String)
+  final case class OutgoingMessage(text: String)
 }
 
-class User(chatRoom: ActorRef) extends Actor {
+class User(chatRoom: ActorRef, nickname: String) extends Actor {
 
   def receive: Receive = {
-    case Connected(nickname, outgoing) =>
+    case Connected(outgoing) =>
       println(s"user created: $nickname")
-      context.become(connected(nickname, outgoing))
+      context.become(connected(outgoing))
       chatRoom ! ChatRoom.Join
   }
 
-  def connected(nickname: String, outgoing: ActorRef): Receive = {
-    case IncomingMessage(author, text) =>
-      chatRoom ! ChatRoom.ChatMessage(author, text)
-    case ChatRoom.ChatMessage(author, text) =>
-      outgoing ! OutgoingMessage(author, text)
+  def connected(outgoing: ActorRef): Receive = {
+    case IncomingMessage(text) =>
+      chatRoom ! ChatRoom.ChatMessage(text)
+    case ChatRoom.ChatMessage(text) =>
+      outgoing ! OutgoingMessage(text)
   }
 
 }
--- src/main/scala/com/sample/chat/WebServer.scala
+++ src/main/scala/com/sample/chat/WebServer.scala
@@ -13,6 +13,8 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.io.StdIn
 
+final case class TextWithAuthor(text: String, author: String)
+
 object WebServer extends App {
 
   def start(): Unit = {
@@ -20,15 +22,15 @@ object WebServer extends App {
 
     val chatRoom = system.actorOf(ChatRoom.props, "chat")
 
-    def newUser(room: ActorRef): Flow[Message, Message, NotUsed] = {
-      val userActor = system.actorOf(Props(new User(room)))
+    def newUser(chatroomRef: ActorRef, nickname: String): Flow[Message, Message, NotUsed] = {
+      val userActor = system.actorOf(Props(new User(chatroomRef, nickname)))
 
       //chatroom's ActorRef is hooked via props of a User, every user must be in a chat room
       //sink, exactly one input, requesting, accepting data elements
       //ability of the webserver actor to receive message from a websocket request
       val incomingMessages: Sink[Message, NotUsed] =
       Flow[Message].map {
-        case TextMessage.Strict(text) => IncomingMessage("nickname", text)
+        case TextMessage.Strict(text) => IncomingMessage(nickname+":"+text)
       }.to(Sink.actorRef[User.IncomingMessage](userActor, PoisonPill, logError))
 
       //user sends to its chatroom actor `outgoing ! OutgoingMessage(author, text)`, therefore triggering
@@ -36,20 +38,21 @@ object WebServer extends App {
       //one time setting up of the source
       //ability of the actor to send message to a chatroom, then sending to all users connected to it
       val outgoingMessages: Source[Message, NotUsed] =
-      Source.actorRef[User.OutgoingMessage](10, OverflowStrategy.fail).mapMaterializedValue { outActor =>
-        userActor ! User.Connected("nickname", outActor)
-        NotUsed
-      }.map((outMsg: User.OutgoingMessage) => TextMessage(outMsg.text))
+      Source.actorRef[User.OutgoingMessage](10, OverflowStrategy.fail)
+        .mapMaterializedValue { outActor =>
+          userActor ! User.Connected(outActor)
+          NotUsed
+        }.map((outMsg: User.OutgoingMessage) => TextMessage(outMsg.text))
 
       Flow.fromSinkAndSource(incomingMessages, outgoingMessages)
     }
 
     //endpoint to send chat, and then broadcasts the message to different user
     val route =
-      path("chat") {
+      path("chat" / Segment / "nickname" / Segment) { (room, nickname) =>
         get {
           //hooking of the actor to a chatroom
-          handleWebSocketMessages(newUser(chatRoom))
+          handleWebSocketMessages(newUser(chatRoom, nickname))
         }
       }
 
@@ -64,8 +67,11 @@ object WebServer extends App {
     case throwable: Throwable => print(s"something went wrong: ${throwable.getMessage}")
   }
 
-  def extractUsernameFlow: Flow[String, Tuple2, NotUsed] = {
-
+  def extractUsernameFlow: Flow[String, TextWithAuthor, NotUsed] = {
+    Flow[String].map { a =>
+      val aaa: Array[String] = a.split("x@x")
+      TextWithAuthor(aaa.head, aaa.tail.toString)
+    }
   }
 
   start()
